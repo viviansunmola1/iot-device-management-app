1bac71a032475230732f9e0b38e634f2f4ebd1f0
Vivian Sunmola <viviansunmola@gmail.com>
2023-09-24 20:49:09 +0100

    Update device creation form layout and handling

--- frontend/src/Devices/Devices.js
+++ frontend/src/Devices/Devices.js
@@ -3,13 +3,24 @@ import axios from 'axios';
 
 const Devices = () => {
   const [devices, setDevices] = useState([]);
-  const [newDevice, setNewDevice] = useState({ name: '', description: '', industry: '' });
-  const [industryData, setIndustryData] = useState([]); // Store industry data
+  const [newDevice, setNewDevice] = useState({
+    name: '',
+    description: '',
+    industry: '', // Initialize industry as an empty string
+    uniqueIdentifier: '', // Initialize uniqueIdentifier as an empty string
+    fee: 0, // Initialize fee as 0
+  });
+  const [industryData, setIndustryData] = useState([]);
+  const [updatingDevice, setUpdatingDevice] = useState(null);
+  const [updatedDeviceData, setUpdatedDeviceData] = useState({
+    name: '',
+    description: '',
+  });
 
   // Fetch industry data when the component mounts
   useEffect(() => {
     axios
-      .get('http://localhost:5000/api/industries') // Replace with your API endpoint
+      .get('http://localhost:5000/api/industries')
       .then((response) => {
         setIndustryData(response.data);
       })
@@ -28,11 +39,45 @@ const Devices = () => {
       .post('http://localhost:5000/api/devices', newDevice)
       .then((response) => {
         setDevices([...devices, response.data]);
-        setNewDevice({ name: '', description: '', industry: '' });
+        setNewDevice({
+          name: '',
+          description: '',
+          industry: '', // Reset industry after creating a device
+          fee: 0, // Reset fee to 0
+        });
       })
       .catch((error) => {
         console.error(error);
       });
+  };
+
+  const handleUpdateDevice = (device) => {
+    setUpdatingDevice(device);
+    setUpdatedDeviceData({
+      name: device.name,
+      description: device.description,
+    });
+  };
+
+  const handleSaveUpdate = () => {
+    axios
+      .put(`http://localhost:5000/api/devices/${updatingDevice._id}`, updatedDeviceData)
+      .then((response) => {
+        const updatedDevices = devices.map((device) =>
+          device._id === response.data._id ? response.data : device
+        );
+        setDevices(updatedDevices);
+        setUpdatingDevice(null);
+        setUpdatedDeviceData({ name: '', description: '' });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleCancelUpdate = () => {
+    setUpdatingDevice(null);
+    setUpdatedDeviceData({ name: '', description: '' });
   };
 
   const handleDeleteDevice = (deviceId) => {
@@ -61,7 +106,7 @@ const Devices = () => {
   return (
     <div>
       <h2>Devices</h2>
-      <div>
+      <div className='heading-space'>
         <h3>Create a New Device</h3>
         <input
           type="text"
@@ -77,6 +122,13 @@ const Devices = () => {
           value={newDevice.description}
           onChange={handleInputChange}
         />
+        <input
+          type="number"
+          name="fee"
+          placeholder="Fee"
+          value={newDevice.fee}
+          onChange={handleInputChange}
+        />
         <select
           name="industry"
           value={newDevice.industry}
@@ -84,7 +136,7 @@ const Devices = () => {
         >
           <option value="">Select Industry</option>
           {industryData.map((industry) => (
-            <option key={industry._id} value={industry.name}>
+            <option key={industry._id} value={industry._id}>
               {industry.name}
             </option>
           ))}
@@ -97,7 +149,32 @@ const Devices = () => {
             <h3>{device.name}</h3>
             <p>{device.description}</p>
             <p>{device.industry}</p>
+            <button onClick={() => handleUpdateDevice(device)}>Update</button>
             <button onClick={() => handleDeleteDevice(device._id)}>Delete</button>
+            {updatingDevice && updatingDevice._id === device._id && (
+              <div>
+                <input
+                  type="text"
+                  name="updatedName"
+                  placeholder="Device Name"
+                  value={updatedDeviceData.name}
+                  onChange={(e) =>
+                    setUpdatedDeviceData({ ...updatedDeviceData, name: e.target.value })
+                  }
+                />
+                <input
+                  type="text"
+                  name="updatedDescription"
+                  placeholder="Device Description"
+                  value={updatedDeviceData.description}
+                  onChange={(e) =>
+                    setUpdatedDeviceData({ ...updatedDeviceData, description: e.target.value })
+                  }
+                />
+                <button onClick={handleSaveUpdate}>Save Update</button>
+                <button onClick={handleCancelUpdate}>Cancel</button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
